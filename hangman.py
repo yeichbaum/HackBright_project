8219b8da5867bddd08a5257a1220cd0b86222e8a
Yasmine Eichbaum <yasmineeichbaum@Yasmines-MacBook-Pro.local>
2017-04-30 21:18:10 -0700

    pseudocode and first commit

---
+++ hangman.py
@@ -0,0 +1,10 @@
+hangman
+
+# two player mode (two human players) or one player mode (player against computer)
+# One player picks a letter/ or guesses a word
+# After player picks letter and/or a word, decide whether that it is correct or wrong
+# Each wrong guess adds to the Hangman
+# Certain amount of wrong answers makes the Hangman
+# Winning conditions: word is completed before the number of wrong possibilities happen 
+#     i.e. even if a player gets 5 wrong guesses, they can still win if they guess the word fi have less than 5 wrong guesses
+# When either the number of wrong guesses reaches 5 or if the player completes the word, the game is over
